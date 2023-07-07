05cb4d2ea7c9b8c9d0ba14ee84d62ff76570df10
jakpra <jp1724@georgetown.edu>
2023-07-07 11:27:00 +0800

    shifted remote/onsite session

--- pages/program.py
+++ pages/program.py
@@ -4,13 +4,15 @@ import csv
 
 global poster_sessions
 poster_sessions = {"Onsite Poster Session 1": [7, 9, 13, 22, 34, 41, 61, 62, 64],
-                   "Remote Poster Session 1": [8, 11, 18, 38, 40, 63],
-                   "Onsite Poster Session 2": [44, 47, 48, 50, 51, 52, 58, 66, 68, 69],
-                   "Remote Poster Session 2": [12, 20, 28, 33, 42, 71]
+                   "Remote Poster Session 1": [8, 11, 18, 27, 38, 40, 63],
+                   "Onsite Poster Session 2": [44, 48, 50, 51, 52, 58, 66, 68, 69],
+                   "Remote Poster Session 2": [12, 20, 28, 33, 42, 47, 71]
                    }
+assert sum(map(len, poster_sessions.values())) == 32
+
 global session_chairs
 session_chairs = {"Remote Poster Session 1": "Djamé Seddah",
-                         "Remote Poster Session 2": "Heike Zinsmeister"}
+                  "Remote Poster Session 2": "Heike Zinsmeister"}
 
 
 global info
